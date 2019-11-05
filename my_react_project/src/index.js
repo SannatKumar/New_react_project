83b7adc6c9f6010ed749f9f2625eb19a66f0f7b1
Raj Kumar <interplaytheheads@gmail.com>
2019-11-05 15:01:48 +0200

    Fixing the css for the body part

--- my_react_project/src/index.js
+++ my_react_project/src/index.js
@@ -4,14 +4,18 @@ import Header from './component/Header.js';
 import Footer from './component/Footer.js';
 import AppRoutes from './Routes.js';
 import { BrowserRouter } from 'react-router-dom';
+import './component/Body.css';
 
 class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
                 <Header />
+                <div className = "routes-footer">
                 <AppRoutes />
+                </div>
                 <Footer />
+             
             </BrowserRouter>
         )
     }
